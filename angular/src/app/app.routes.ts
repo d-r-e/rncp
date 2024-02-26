6b27f4cca02fc7bb2105acd89f2ff5da4b0cf500
David Rodríguez Elipe <david.rodriguezelipe@telefonica.com>
2024-02-26 22:32:46 +0100

    faq, fixes y bonus

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PathComponent } from './path/path.component';
 import { AuthGuardService } from './services/auth-guard.service';
+import { FaqComponent } from './faq/faq.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,9 @@ export const routes: Routes = [
   },
   {
     path: 'login', component: LoginComponent
+  },
+  {
+    path: 'faq', component: FaqComponent, canActivate: [AuthGuardService],
   },
   {
     path: 'auth/callback', component: LoginComponent
---
+++ angular/src/app/path/vertical-slider/vertical-slider.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-slider',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './vertical-slider.component.html',
+  styleUrl: './vertical-slider.component.css'
+})
+export class VerticalSliderComponent {
+  @Input() bindElement!: HTMLElement;
+  @Input() min: number = 0;
+  @Input() max: number = 100;
+  @Input() step: number = 1;
+  @Output() valueChange = new EventEmitter<number>();
+
+  public value: number;
+  public showSlider: boolean = true;
+  public showValue: boolean = false;
+
+  constructor() {
+    this.value = this.min;
+  }
+
+  onValueChange(newValue: number) {
+    this.value = newValue;
+    this.valueChange.emit(this.value);
+    this.showValue = true;
+  }
+
+  toggleSliderDisplay() {
+    this.showSlider = !this.showSlider;
+  }
+
+  onSliderLeave() {
+    this.showValue = false;
+  }
+}
