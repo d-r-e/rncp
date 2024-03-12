69f29aebea8adccf598dd29aead5aa823f549e23
¨Axel <axel.coezard@gmail.com>
2024-03-12 11:11:12 +0100

    merge main into local

--- angular/src/environments/environment.development.ts
+++ angular/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  auth_url: "https://api.intra.42.fr/oauth/authorize?client_id=<CLIENT_UUID>&redirect_uri=<REDIRECT_URI>&response_type=code",
+  auth_url: "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-e325cc8b1c7ab1e70b093c803d91154df31ce6d0f7378dc03f249c801a8e3314&redirect_uri=http://localhost:8000/login&response_type=code",
   api_url: "https://api.intra.42.fr"
 };
